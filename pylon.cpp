ebc7274f7fe396d04bcba26a8506b24adc353723
FreddyZeng <freddyzeng@FreddyZengs-Mac-mini.local>
2025-06-22 07:02:19 +0800

    fix port

--- pylon.cpp
+++ pylon.cpp
@@ -14,7 +14,7 @@
 #include "Phy.hpp"
 
 #define ZT_TCP_PROXY_CONNECTION_TIMEOUT_SECONDS 300
-#define ZT_TCP_PROXY_TCP_PORT                   58443
+#define ZT_TCP_PROXY_TCP_PORT                   443
 #define INVALID_SOCKET_FD                       -1
 
 #include "ZeroTierSockets.h"
